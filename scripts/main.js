896b2d1aa79a6c1d98389030a3c7b8dfdb6bfe44
Welton Vaz <weltonvaz@gmail.com>
2023-11-18 22:31:43 -0300

    Update do branch gh-pages e reestrutaração pastas e arquivos - parte 2

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+async function converter() {
+    try {
+        const response = await fetch('https://open.er-api.com/v6/latest/USD');
+        if (!response.ok) {
+            throw new Error('Erro ao obter a cotação do dólar. Verifique a conexão ou tente novamente mais tarde.');
+        }
+
+        const data = await response.json();
+        const cotacaoDolar = data.rates.USD;
+
+        var valorEmDolar = parseFloat(document.getElementById("dolarInput").value);
+        
+        if (isNaN(valorEmDolar)) {
+            throw new Error('Por favor, insira um valor válido em dólar.');
+        }
+
+        var valorEmReais = valorEmDolar * cotacaoDolar;
+
+        document.getElementById("reaisOutput").value = valorEmReais.toFixed(2);
+    } catch (error) {
+        console.error(error.message);
+        alert(error.message);
+    }
+}
